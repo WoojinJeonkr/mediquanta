7d4857c80cd0beadfe6eba91a4cd261496a99ddd
WoojinJeonkr <wooeric1@naver.com>
2024-06-03 10:21:38 +0900

    init project

---
+++ mips/src/test/java/com/application/mips/MipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.mips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
